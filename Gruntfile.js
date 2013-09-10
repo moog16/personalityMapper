80c7cf2b7c6271bc8e852eeeed6c23c6c4d3555e
Matthew Goo <mattgoo@gmail.com>
2013-09-09 21:04:28 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg:grunt.file.readJSON('package.JSON'),
+
+    nodemon: {
+      dev: {}
+    },
+    watch: {
+      js: {
+        files: ['app.js']
+      } //,
+      // html: {
+      //   // files: ['index.html']
+      // }
+    },
+    concurrent: {
+      target: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['concurrent:target']);
+};
---
+++ public/javascripts/ajaxTest.js
@@ -0,0 +1,31 @@
+// var urlPath = '1043010258?fields=name,about,friends.fields(name)';
+// // '1043010258?fields=id,name';
+
+
+
+
+// $.ajax('https://graph.facebook.com/'+urlPath, {
+//   contentType: 'application/json',
+//   success: function(data){
+//         console.log(data);
+//         //$('#messageWrapper').append($message).append('<br />');
+//       },
+//     error: function(data) {
+//       console.log('Ajax request failed');
+//     }
+// });
+
+
+//     // Load the SDK asynchronously
+//     (function(d, s, id){
+//        var js, fjs = d.getElementsByTagName(s)[0];
+//        if (d.getElementById(id)) {return;}
+//        js = d.createElement(s); js.id = id;
+//        js.src = "http://connect.facebook.net/en_US/all.js"; // en_US/all/debug.js = non minified
+//        fjs.parentNode.insertBefore(js, fjs);
+//      }(document, 'script', 'facebook-jssdk'));
+//   </script>
+
+
+// </body>
+// </html> 
