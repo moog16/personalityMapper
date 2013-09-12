4175df32fb5f96f552ef1d86b3b882a6f8199599
Matthew Goo <mattgoo@gmail.com>
2013-09-11 17:57:56 -0700

    doing some more moving

--- routes/hasLogin.js
+++ routes/hasLogin.js
@@ -7,14 +7,17 @@ exports.login = function(req, res) {
       headers:  { connection:  "keep-alive" }
   };
 
+  var d;
   graph
     .setOptions(options)
     .get("me?fields=id,name,friends.fields(name)", function(err, res) {
       console.log(res.friends.data);
+      //instead of console.log....res.send
+      d = res.friends.data;
       // for(var i=0; i<res.friends.data.length; i++) {
       //   console.log(res.friends.data[i].name);
       // }
     });
 
-  res.render("index", { title: "Logged In" });
+  res.render("index", { title: "Logged In", names: d });
 };
